72d7d1f865c9a1b549226d842a9096a0aca80640
yellow-sky <nikulin.e@gmail.com>
2014-12-18 12:37:18 +0300

    Build script chaingings (remove contribs)

--- build_pack.sh
+++ build_pack.sh
@@ -19,6 +19,10 @@ cp -R ./src/* $BUILD_DIR/$PLUGIN_NAME
 #Clean
 rm $BUILD_DIR/$PLUGIN_NAME/*.pyc
 
+#Remove Contrib
+rm -R $BUILD_DIR/$PLUGIN_NAME/data_sources_contrib
+rm -R $BUILD_DIR/$PLUGIN_NAME/groups_contrib
+
 #Get version
 cd $BUILD_DIR
 VER=`grep "version=" ./$PLUGIN_NAME/metadata.txt | sed 's/version=//'`
