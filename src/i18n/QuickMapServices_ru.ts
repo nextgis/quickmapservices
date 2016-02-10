18a8c8071be856b7ab4a881631a2ff101885bb5c
alisovenko <alexander.lisovenko@gmail.com>
2016-02-11 00:01:01 +0300

    Fix translation

--- src/i18n/QuickMapServices_ru.ts
+++ src/i18n/QuickMapServices_ru.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="ru" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -28,7 +29,7 @@
     <message>
         <location filename="data_sources_model.py" line="32"/>
         <source>Group/DS</source>
-        <translation>Группа\Подолжка</translation>
+        <translation>Группа\Подложка</translation>
     </message>
 </context>
 <context>
