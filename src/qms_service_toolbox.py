ada83704cdf2578eefe81a98ad082a7e868fa7f4
alisovenko <alexander.lisovenko@gmail.com>
2016-06-14 23:00:17 +0300

    Fix #87

--- src/qms_service_toolbox.py
+++ src/qms_service_toolbox.py
@@ -3,12 +3,13 @@ from os import path
 
 from PyQt4 import uic
 from PyQt4.QtGui import QDockWidget, QListWidgetItem, QCursor, QApplication # , QMessageBox
-from PyQt4.QtCore import QThread, pyqtSignal, Qt, QTimer
+from PyQt4.QtCore import QThread, pyqtSignal, Qt, QTimer, QMutex
 
 from data_source_serializer import DataSourceSerializer
 from qgis_map_helpers import add_layer_to_map
 from .qms_external_api_python.client import Client
 import sys
+
 
 FORM_CLASS, _ = uic.loadUiType(path.join(
     path.dirname(__file__), 'qms_service_toolbox.ui'))
@@ -18,7 +19,7 @@ class QmsServiceToolbox(QDockWidget, FORM_CLASS):
     def __init__(self, iface):
         QDockWidget.__init__(self, iface.mainWindow())
         self.setupUi(self)
-        
+
         self.iface = iface
         self.search_threads = None  # []
 
@@ -34,21 +35,21 @@ class QmsServiceToolbox(QDockWidget, FORM_CLASS):
 
         self.lstSearchResult.itemDoubleClicked.connect(self.result_selected)
 
+        self.one_process_work = QMutex()
 
     def start_search(self):
-        print 'start search'
         search_text = unicode(self.txtSearch.text())
         if not search_text:
             self.lstSearchResult.clear()
             return
 
-        if 1 == 1 and self.search_threads:
-            print 'Kill ', self.search_threads
-            self.search_threads.terminate()
-            self.search_threads.wait()
-            
+        # if 1 == 1 and self.search_threads:
+        #     # print 'Kill ', self.search_threads
+        #     self.search_threads.terminate()
+        #     self.search_threads.wait()
+
         self.show_progress()
-        searcher = SearchThread(search_text, self.iface.mainWindow())
+        searcher = SearchThread(search_text, self.one_process_work, self.iface.mainWindow())
         searcher.data_downloaded.connect(self.show_result)
         searcher.error_occurred.connect(self.show_error)
         self.search_threads = searcher
@@ -57,15 +58,17 @@ class QmsServiceToolbox(QDockWidget, FORM_CLASS):
     def show_progress(self):
         self.lstSearchResult.clear()
         self.lstSearchResult.addItem(self.tr('Searching...'))
-        
+
     def show_result(self, results):
         self.lstSearchResult.clear()
         if results:
             for geoservice in results:
                 new_item = QListWidgetItem()
-                new_item.setText(unicode(geoservice['name']) +' [%s]' % geoservice['type'].upper())
+                new_item.setText(
+                    unicode(geoservice['name']) + ' [%s]' % geoservice['type'].upper()
+                )
                 new_item.setData(Qt.UserRole, geoservice)
-                #todo: remake with cache icons
+                # todo: remake with cache icons
                 self.lstSearchResult.addItem(new_item)
         else:
             new_item = QListWidgetItem()
@@ -74,9 +77,9 @@ class QmsServiceToolbox(QDockWidget, FORM_CLASS):
             self.lstSearchResult.addItem(new_item)
 
         self.lstSearchResult.update()
-            
+
     def show_error(self, error_text):
-        #print error_text
+        # print error_text
         self.lstSearchResult.clear()
         self.lstSearchResult.addItem(error_text)
 
@@ -94,35 +97,33 @@ class QmsServiceToolbox(QDockWidget, FORM_CLASS):
                 QApplication.restoreOverrideCursor()
 
 
-
-
-
-
 class SearchThread(QThread):
 
     data_downloaded = pyqtSignal(object)
-    error_occurred = pyqtSignal(object)    
-    
-    def __init__(self, search_text, parent=None):
+    error_occurred = pyqtSignal(object)
+
+    def __init__(self, search_text, mutex, parent=None):
         QThread.__init__(self, parent)
         self.search_text = search_text
         self.searcher = Client()
+        self.mutex = mutex
 
     def run(self):
         results = []
 
         # search
         try:
+            self.mutex.lock()
             results = self.searcher.search_geoservices(self.search_text)
+
+            self.data_downloaded.emit(results)
         except URLError:
                         error_text = (self.tr("Network error!\n{0}")).format(unicode(sys.exc_info()[1]))
-                        #error_text = 'net'
+                        # error_text = 'net'
                         self.error_occurred.emit(error_text)
-                        return
         except Exception:
                         error_text = (self.tr("Error of processing!\n{0}: {1}")).format(unicode(sys.exc_info()[0].__name__), unicode(sys.exc_info()[1]))
-                        #error_text = 'common'
+                        # error_text = 'common'
                         self.error_occurred.emit(error_text)
-                        return
 
-        self.data_downloaded.emit(results)
+        self.mutex.unlock()
