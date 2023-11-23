f0644538c533bab4da326e84a40868461e728f23
Ivan Barsukov <i.barsukov@protonmail.com>
2023-11-23 22:57:59 +0700

    Update pylupdate

--- update_translates.sh
+++ update_translates.sh
@@ -6,17 +6,15 @@ TRANSLATE_FILES=`find ./src/ -iname "*.py" -o -iname "*.ui"`
 #pylupdate4 -noobsolete ./src/*.{py,ui} -ts ./src/i18n/base.ts
 
 #Update translate files
-pylupdate4 -noobsolete $TRANSLATE_FILES -ts ./src/i18n/*.ts
+pylupdate5 -noobsolete $TRANSLATE_FILES -ts ./src/i18n/*.ts
 echo "TS files have been updated!"
 
 #Check
 for file in `ls ./src/i18n/*.ts|grep -v base.ts`
-do 
+do
 str_to_update=`grep -c 'type="unfinished"' $file`
 if [ $str_to_update -gt 0 ];
 then
       echo "Need to retranslate: $file ($str_to_update string(s))"
 fi
 done
-
-
